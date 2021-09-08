84e491ebe386382b8cce6e8cda652c51ebda0447
Jéssika <jessikafujimura@gmail.com>
2021-09-07 21:01:43 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer(params: any) {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/JessikaFujimura" target="_blank" rel="noreferrer">Jéssika Fujimura</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/jessikafujimura.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
