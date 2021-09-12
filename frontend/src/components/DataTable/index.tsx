3399cb90e7b92e4728f616535545e332475b68a3
Jéssika <jessikafujimura@gmail.com>
2021-09-12 17:37:26 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/Format";
+import { BASE_URL } from "utils/requests";
+
 function DataTable(params: any) {
+
+    const [page, setPage] = useState<SalePage>({
+        last: true,
+        totalElements: 0,
+        totalPages: 0,
+        first: true,
+        number: 0,
+    })
+
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?size=20&page=0&sort=date,asc`)
+        .then(response => {
+            setPage(response.data)
+        })
+    }, [])
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -12,20 +35,15 @@ function DataTable(params: any) {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                    {page.content?.map(i => 
+                    <tr key={i.id}>
+                        <td>{formatLocalDate(i.date, "dd/MM/yyyy")}</td>
+                        <td>{i.seller.name}</td>
+                        <td>{i.visited}</td>
+                        <td>{i.deals}</td>
+                        <td>{i.amount.toFixed(2)}</td>
                     </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
+                    )}
                 </tbody>
             </table>
         </div>
